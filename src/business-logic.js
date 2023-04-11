bd6b5e3786d8859176f52a54888e482a2309f5a6
MarcusKyung <Kyungmj@gmail.com>
2023-04-11 13:49:09 -0700

    add files needed for dev environment

--- src/business-logic.js
+++ src/business-logic.js
@@ -1,3 +1,4 @@
+
 //Game setup/variables:
 export let player1 = {
   turnScore: 0,
@@ -12,6 +13,19 @@ export let player2 = {
 };
 
 //Business Logic
+export function checkGameOver() {
+  if (player1.gameScore >= 100) {
+    document.getElementById("gameOver").innerText = "Game Over! Player 1 wins!";
+    document.querySelector("div#rollOption").removeEventListener("click", rollDice);
+    document.querySelector("div#holdOption").removeEventListener("click", hold);
+  } 
+  else if (player2.gameScore >= 100) {
+    document.getElementById("gameOver").innerText = "Game Over! Player 2 wins!";
+    document.querySelector("div#rollOption").removeEventListener("click", rollDice);
+    document.querySelector("div#holdOption").removeEventListener("click", hold);
+  }
+}
+
 export function swapPlayers(){
   if (player1.isActive === true) {
     player1.isActive = false;
@@ -21,6 +35,24 @@ export function swapPlayers(){
     player2.isActive = false;
   }
 }
+
+export function hold(){
+  if (player1.isActive === true) {
+    player1.gameScore = player1.gameScore + player1.turnScore;
+    document.getElementById("p1Total").innerText = "Player 1 Game Total: " + player1.gameScore;
+    player1.turnScore = 0;
+    document.getElementById("p1Turn").innerText = "Player 1 Turn Total: "+ player1.turnScore;
+    swapPlayers();
+    checkGameOver();
+  } else if (player2.isActive === true) {
+    player2.gameScore = player2.gameScore + player2.turnScore;
+    document.getElementById("p2Total").innerText = "Player 2 Game Total: " + player2.gameScore;
+    player2.turnScore = 0;
+    document.getElementById("p2Turn").innerText = "Player 2 Turn Total: "+ player2.turnScore;
+    swapPlayers();
+    checkGameOver();
+  }
+} 
 
 export function rolledOne(){
   if (player1.isActive === true) {
@@ -36,33 +68,30 @@ export function rolledOne(){
   } 
 }
 
-export function hold(){
-  if (player1.isActive === true) {
-  player1.gameScore = player1.gameScore + player1.turnScore;
-  document.getElementById("p1Total").innerText = "Player 1 Game Total: " + player1.gameScore;
-  player1.turnScore = 0;
-  document.getElementById("p1Turn").innerText = "Player 1 Turn Total: "+ player1.turnScore;
-  swapPlayers();
-  checkGameOver();
-  } else if (player2.isActive === true) {
-    player2.gameScore = player2.gameScore + player2.turnScore;
-    document.getElementById("p2Total").innerText = "Player 2 Game Total: " + player2.gameScore;
-    player2.turnScore = 0;
-    document.getElementById("p2Turn").innerText = "Player 2 Turn Total: "+ player2.turnScore;
-    swapPlayers();
-    checkGameOver();
-  }
-} 
+export function diceCalculation() {
+  return Math.floor(Math.random() * 6) + 1; 
+}
 
-export function checkGameOver() {
-  if (player1.gameScore >= 100) {
-    document.getElementById("gameOver").innerText = "Game Over! Player 1 wins!";
-    document.querySelector("div#rollOption").removeEventListener("click", rollDice);
-    document.querySelector("div#holdOption").removeEventListener("click", hold);
-  } 
-  else if (player2.gameScore >= 100) {
-    document.getElementById("gameOver").innerText = "Game Over! Player 2 wins!";
-    document.querySelector("div#rollOption").removeEventListener("click", rollDice);
-    document.querySelector("div#holdOption").removeEventListener("click", hold);
+export function rollDice() {
+  const currentRoll = diceCalculation();
+  document.getElementById("currentDiceRoll").innerText = "You rolled a: " + currentRoll;
+  if (player1.isActive === true){
+    if (currentRoll !== 1) {
+      player1.turnScore = player1.turnScore + currentRoll;
+      document.getElementById("p1Turn").innerText = "Player 1 Turn Total: "+ player1.turnScore;
+    } else {
+      player1.turnScore = 0;
+      document.getElementById("p1Turn").innerText = "Player 1 Turn Total: "+ player1.turnScore;
+      rolledOne();
+    }
+  } else if (player1.isActive === false) {
+    if (currentRoll !== 1) {
+      player2.turnScore = player2.turnScore + currentRoll;
+      document.getElementById("p2Turn").innerText = "Player 2 Turn Total: "+ player2.turnScore;
+    } else {
+      player2.turnScore = 0;
+      document.getElementById("p2Turn").innerText = "Player 2 Turn Total: "+ player2.turnScore;
+      rolledOne();
+    }
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,38 +1,11 @@
 //Imports
-import { swapPlayers, rolledOne, hold, checkGameOver } from './business-logic.js';
-
-import { player1, player2} from './business-logic.js';
+import { rollDice, hold } from './business-logic.js';
+import './css/styles.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
 
 //User Logic
-function diceCalculation() {
-  return Math.floor(Math.random() * 6) + 1; 
-}
-
-function rollDice() {
-  const currentRoll = diceCalculation();
-  console.log(currentRoll);
-  document.getElementById("currentDiceRoll").innerText = "You rolled a: " + currentRoll;
-  if (player1.isActive === true){
-    if (currentRoll !== 1) {
-      player1.turnScore = player1.turnScore + currentRoll;
-      document.getElementById("p1Turn").innerText = "Player 1 Turn Total: "+ player1.turnScore;
-    } else {
-      player1.turnScore = 0;
-      document.getElementById("p1Turn").innerText = "Player 1 Turn Total: "+ player1.turnScore;
-      rolledOne();
-    }
-  } else if (player1.isActive === false) {
-    if (currentRoll !== 1) {
-      player2.turnScore = player2.turnScore + currentRoll;
-      document.getElementById("p2Turn").innerText = "Player 2 Turn Total: "+ player2.turnScore;
-    } else {
-      player2.turnScore = 0;
-      document.getElementById("p2Turn").innerText = "Player 2 Turn Total: "+ player2.turnScore;
-      rolledOne();
-    }
-  }
-}
-
 window.addEventListener("load", function (){
   document.querySelector("div#rollOption").addEventListener("click", rollDice);
   document.querySelector("div#holdOption").addEventListener("click", hold);
